2728769d0646b3c6c19fb67203b7972fc9398426
Vinay Varma <vinayvarma5208@gmail.com>
2025-03-06 16:33:48 -0500

    first commit

---
+++ src/main/java/com/vinay/quizapp/MicroservicesProjectApplication.java
@@ -0,0 +1,14 @@
+package com.vinay.quizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesProjectApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(MicroservicesProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vinay/quizapp/MicroservicesProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.quizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
